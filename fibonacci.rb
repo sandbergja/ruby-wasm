f1e4bed235c4b42bd6a81a560c3eeae95cc84c3b
Jane Sandberg <js7389@princeton.edu>
2023-02-07 16:02:11 -0800

    working example

---
+++ fibonacci.rb
@@ -0,0 +1,11 @@
+class Fibonacci
+  def initialize(current_sequence)
+    @two_most_recent = current_sequence.last(2)
+                                       .map(&:to_i)
+  end
+
+  def new_number
+    return 1 unless @two_most_recent.length == 2
+    @two_most_recent.sum
+  end
+end
