3dad2b7951a98c5d36a6418a0abc5d36a8cfa200
Jane Sandberg <js7389@princeton.edu>
2023-02-14 14:37:48 -0800

    Add rubocop

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
+# A class that, given an array of numbers,
+# finds the next one in the Fibonacci sequence
 class Fibonacci
   def initialize(current_sequence)
     @two_most_recent = current_sequence.last(2)
@@ -6,6 +10,7 @@ class Fibonacci
 
   def new_number
     return 1 unless @two_most_recent.length == 2
+
     @two_most_recent.sum
   end
 end
--- handle_button.rb
+++ handle_button.rb
@@ -1,16 +1,18 @@
+# frozen_string_literal: true
+
 # The JS module from here: https://github.com/ruby/ruby.wasm/blob/main/ext/js/lib/js.rb
 require 'js'
 
 document = JS.global[:document]
 
-document.getElementById('button').addEventListener("click") do |event|
+document.getElementById('button').addEventListener('click') do |_event|
   # Get the contents of the DOM
   current_list = document[:body][:innerText].to_s.split("\n")
   # Remove the button text
   current_list.reject! { |element| element == 'Add next fibonnaci number' }
   new_number = Fibonacci.new(current_list).new_number
 
-  li = JS.global[:document].createElement("li")
+  li = JS.global[:document].createElement('li')
   li[:innerText] = new_number
   document.getElementById('list').appendChild(li)
 end
