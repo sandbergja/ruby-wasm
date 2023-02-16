4975fd10b64c50c0b2793d1d6ba3f37e43805e84
Max Kadel <mkadel@princeton.edu>
2023-02-16 15:41:49 -0500

    Update activity
    Co-authored-by: Christina Chortaria <christinach@users.noreply.github.com>
    Co-authored-by: Jane Sandberg <sandbergja@users.noreply.github.com>

--- activity/my_class.rb
+++ activity/my_class.rb
@@ -1,27 +1,24 @@
 # frozen_string_literal: true
 
 require 'json'
-require 'net/http'
 
 # The best class there ever was!
+# Things that don't seem to work:
+# - Network calls
+# - Filesystem navigation
 class MyClass
-  def initialize
-    fetch_oaks
-  end
+  # def initialize
+  #   fetch_oaks
+  # end
 
   def to_s
-    items = @data.map { |item| "<li><a href='#{item['uri']}'>#{item['place_guess']}</a></li>" }
-    "<ul>#{items.join}</ul>"
+    "*<br>"
   end
 
   private
 
-  def fetch_oaks
-    uri = URI('http://www.inaturalist.org/observations.json?taxon_name=quercus&quality_grade=research')
-    Net::HTTP.start(uri.host, uri.port) do |http|
-      request = Net::HTTP::Get.new uri
-      response = http.request request
-      @data = JSON.parse(response.body)
-    end
-  end
+  # def fetch_oaks
+  #   file = File.open "activity.json"
+  #   @data = JSON.load file
+  # end
 end
